b2bb3a4ab70295841bc759c6df31e4afca336b65
chomieu <chomieu@gmail.com>
2021-02-16 14:08:38 -0800

    Search function

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import SearchForm from "../components/SearchForm";
+import API from "../utils/API";
 
-function Search() {
+class Search extends Component {
+  state = {
+    search: "",
+  };
 
-  return (
-    <div className="container">
-      <h2 className="text-center">Search By Breed!</h2>
-      <form>
-        <div className="form-group">
-          <label for="breed">Breed Name:</label>
-          <input className="form-control" id="breed" placeholder="Type in a dog breed to begin"></input>
-        </div>
-        <button type="submit" className="btn btn-success">Search</button>
-      </form>
-    </div>
-  );
+  handleInputChange = event => {
+    const name = event.target.name
+    const value = event.target.value
+    this.setState({ [name]: value })
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault()
+    this.searchMovies(this.state.search)
+    this.setState({ search: "" })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h2 className="text-center">Search By Breed!</h2>
+        <SearchForm
+          value={this.state.search}
+          handleInputChange={this.handleInputChange}
+          handleFormSubmit={this.handleFormSubmit}
+        />
+      </div>
+    );
+  }
 }
 
 export default Search;
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-const BASEURL = "https://dog.ceo/api/breeds/image/random";
+const BASEURL = "https://dog.ceo/api/";
 
 export default {
   get: function () {
-    return axios.get(BASEURL);
+    return axios.get(BASEURL + "/image/random");
   },
+  search: function(query) {
+    return axios.get(BASEURL + query + "/image/random");
+  }
 };
