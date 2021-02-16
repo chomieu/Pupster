0d4f227854b5ecb5a3d160b3e6648fc4fc94d940
chomieu <chomieu@gmail.com>
2021-02-16 13:10:59 -0800

    Removed unnecessary dependencies and Home route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
-import Home from "./pages/About";
 import About from "./pages/About";
 import Discover from "./pages/Discover";
 import Search from "./pages/Search";
@@ -11,8 +10,7 @@ function App() {
     <Router>
       <div>
         <NavTabs />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
+        <Route exact path="/" component={About} />
         <Route exact path="/discover" component={Discover} />
         <Route exact path="/search" component={Search} />
       </div>
