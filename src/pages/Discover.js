845447f5bdac5a46b66ebc19650485664942f09e
RitaZ <xiaoyz109@gmail.com>
2021-02-16 13:13:38 -0800

    pull

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,14 +1,26 @@
 import React from "react";
+import NavTabs from "../components/NavTabs";
 
 export default function Discover() {
-  return <div>
-    <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-  </div>;
+  return (
+    <div>
+      <NavTabs />
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
